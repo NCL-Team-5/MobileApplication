4c48c8340f107d61f49a9b0fc2f77193ddc89dcf
Said Haj-Hassan <s.haj-hassan@ncl.ac.uk>
2020-02-28 17:14:44 +0000

    Add project files.

---
+++ SaggezzaApp/SaggezzaApp/SettingsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SaggezzaApp {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage {
+        public SettingsPage() {
+            InitializeComponent();
+        }
+
+        void HelpClicked(object sender, EventArgs e) {
+
+        }
+
+        void LogoutClicked(object sender, EventArgs e) {
+
+        }
+    }
+}
