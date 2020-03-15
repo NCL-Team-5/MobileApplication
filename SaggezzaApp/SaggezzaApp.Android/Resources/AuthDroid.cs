2619a6e657f2e44bc7ca76fca3fcb9e33163eede
BulsonJ <jackthomasbulson@gmail.com>
2020-03-15 15:58:36 +0000

    Prerequisite for Basic Sign in Functionality

---
+++ SaggezzaApp/SaggezzaApp.Android/Resources/AuthDroid.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+using SaggezzaApp;
+using SaggezzaApp.Droid;
+using Firebase.Auth;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AuthDroid))]
+namespace SaggezzaApp.Droid
+{
+    public class AuthDroid : IAuth
+    {
+        public async Task<string> LoginWithEmailPassword(string email, string password)
+        {
+            try
+            {
+                var user = await FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+                var token = await user.User.GetIdTokenAsync(false);
+                return token.Token;
+            }
+            catch (FirebaseAuthInvalidUserException e)
+            {
+                e.PrintStackTrace();
+                return "";
+            }
+        }
+    }
+}
