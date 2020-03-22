f9637f38d8183a9ab2dc01e05e2dbee3a52c55ca
Rachel Swift <b8024117@newcastle.ac.uk>
2020-03-22 12:47:15 +0000

    This is the addidtion of the navigation, where apges are visually complete the action bar has been removed. On pages past receipts and attatchments navigation as not been addded as there is still alot of work to do backend but oyu can navigate back with the action bar

--- SaggezzaApp/SaggezzaApp/App.xaml.cs
+++ SaggezzaApp/SaggezzaApp/App.xaml.cs
@@ -6,8 +6,7 @@ namespace SaggezzaApp {
     public partial class App : Application {
         public App() {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new HomePage());
         }
 
         protected override void OnStart() {
---
+++ SaggezzaApp/SaggezzaApp/HelpPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SaggezzaApp {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HelpPage : ContentPage {
+        public HelpPage() {
+            InitializeComponent();
+        }
+
+        private async void Home(object sender, EventArgs e) {
+            var page = new HomePage();
+            await Navigation.PushAsync(page);
+        }
+    }
+}
