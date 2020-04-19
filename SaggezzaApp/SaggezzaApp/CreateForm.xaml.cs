29f9cfc5dcb503fbc8c91f500fc0d1d46fbf32de
Rachel Swift <b8024117@newcastle.ac.uk>
2020-04-19 12:36:08 +0100

    Nav of create form, error in past receipts fixed

--- SaggezzaApp/SaggezzaApp/CreateForm.xaml.cs
+++ SaggezzaApp/SaggezzaApp/CreateForm.xaml.cs
@@ -13,5 +13,27 @@ namespace SaggezzaApp {
         public CreateForm() {
             InitializeComponent();
         }
+
+        private async void Home(object sender, EventArgs e) {
+            var page = new HomePage();
+            await Navigation.PushAsync(page);
+        }
+
+        private async void PastReceipts(object sender, EventArgs e) {
+            var page = new PastReceipts();
+            await Navigation.PushAsync(page);
+        }
+
+        
+
+        private async void Attatchments(object sender, EventArgs e) {
+            var page = new Attatchments();
+            await Navigation.PushAsync(page);
+        }
+
+        private async void Profile(object sender, EventArgs e) {
+            var page = new ProfilePage();
+            await Navigation.PushAsync(page);
+        }
     }
 }
